de9a98838124fc72cac6b62ad26a339229885ee9
Freyashingala <152647083+Freyashingala@users.noreply.github.com>
2025-02-25 17:37:32 +0530

    Updates

--- client/src/Context/AuthContext.js
+++ client/src/Context/AuthContext.js
@@ -2,7 +2,7 @@ import React, { createContext, useContext, useState } from "react";
 import axios from "axios";
 
 const AuthContext = createContext();
-const API_URL = "http://localhost:5000";
+const API_URL = "http://localhost:5000" || "https://gemini-intelligent-ide.onrender.com";
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
